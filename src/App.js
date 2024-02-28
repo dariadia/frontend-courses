7bd77a9984bc821fcb5388f4cd4eeda0f4c0e68b
Daria <daria.diachkova@mail.ru>
2024-02-29 01:34:11 +0300

    add courses

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const ReactCourse = () => {
   const { t } = useTranslation()
   return (<section className="course-section"><h3>React</h3>
     <h4>{t('react_base')}</h4>
-    <ol>{[1, 2, 3, 4].map(item => <li key={item}>
+    <ol>{[1, 2, 3, 4, 5, 6,7,8,9].map(item => <li key={item}>
       <details>
         <summary>{t(`react_base_${item}`)}</summary>
         <div>{t(`react_base_${item}_details`)}</div>
