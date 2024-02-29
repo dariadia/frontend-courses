61d2d452b26f69de0c9d5a62f66d0ff121ebb544
Daria <daria.diachkova@mail.ru>
2024-02-29 03:31:30 +0300

    add tabs

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           <h2>{t('what_you_get')}</h2>
           <p>{t('personalised')}</p>
         </section>
-        {course === 'React' ? <ReactCourse /> : <JsCourse />}
+        {course === 'React' ? <ReactCourse setCourse={setCourse} /> : <JsCourse setCourse={setCourse} />}
       </main>
       <footer>
         <a target="_blank" href="https://github.com/dariadia/" rel="noreferrer">Github @dariadia</a>
@@ -37,17 +37,21 @@ function App() {
   )
 }
 
-const ReactCourse = () => {
+const ReactCourse = ({ setCourse }) => {
   const { t } = useTranslation()
-  return (<section className="course-section"><h3>React</h3>
+  return (<section className="course-section">
+    <div className="flex-box">
+      <h3>React</h3>
+      <h3 onClick={() => setCourse('JavaScript')} className="disabled-tab">JavaScript</h3>
+    </div>
     <h4>{t('react_base')}</h4>
     <ol>{[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => <li key={item}>
       <details>
         <summary>{t(`react_base_${item}`)}</summary>
         <div>{t(`react_base_${item}_details`)}</div>
       </details>
-
-    </li>)}</ol>
+    </li>)}
+    </ol>
 
     <h4>{t('react_adv')}</h4>
     WIP
@@ -61,8 +65,14 @@ const ReactCourse = () => {
   </section>)
 }
 
-const JsCourse = () => {
-  return (<section><h3>JavaScript</h3></section>)
+const JsCourse = ({ setCourse }) => {
+  return (<section>
+    <div className="flex-box">
+      <h3>JavaScript</h3>
+      <h3 onClick={() => setCourse('React')} className="disabled-tab">React.js</h3>
+    </div>
+  </section>
+  )
 }
 
 
